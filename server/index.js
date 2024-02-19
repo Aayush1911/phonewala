dcd39e4f9f7781a252b39371580d9369b2c6805e
Aayush1911 <21it49@svitvasad.ac.in>
2024-02-19 21:35:19 +0530

    Client components made

--- server/index.js
+++ server/index.js
@@ -2,11 +2,17 @@ const express=require('express');
 const connectToMongo = require('./db');
 const app=express();
 app.use(express.json())
+var cors = require('cors')
+
 const port=4000;
 connectToMongo()
 app.listen(port,()=>{
     console.log('App listening on port 4000');
 })
-
+app.use(cors({
+    orgin:["http://localhost:5173/"],
+    methods:["POST","GET"],
+    credentials:true
+  }))
 app.use('/auth',require('./routes/authroute'));
 app.use('/mobile',require('./routes/mobileroute'));

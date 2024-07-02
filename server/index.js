0fef05576713bdea43eb63c391216e8be8d29fc2
Aayush1911 <21it49@svitvasad.ac.in>
2024-07-02 09:31:09 +0530

    update index.js

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(session({
 // CORS setup
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://phonewalaclient.vercel.app' // Add your production client URL here
+  'https://phonewala-ecoommerce.vercel.app/' // Add your production client URL here
 ];
 
 const corsOptions = {
